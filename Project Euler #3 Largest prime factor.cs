9e69ce146aadad303d1472e580d1ec8bca70d569
Aman Ahuja <trcxr16@hotmail.com>
2015-06-08 02:49:39 +0530

    Euler3
    Language C#

---
+++ Project Euler #3 Largest prime factor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.ProjectEuler
+{
+    class Euler3
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                long N = long.Parse(Console.ReadLine());
+
+                long temp = N;
+                for (long j = 2; j <= Math.Sqrt(N); j++)
+                {
+                    if (j == temp)
+                    {
+                        break;
+                    }
+                    if (temp % j == 0)
+                    {
+                        temp = temp / j;
+                        j--;
+                    }
+                }
+                Console.WriteLine(temp);
+            }
+            Console.Read();
+        }
+    }
+}
