2f572fff7bc6b5ca251af87fc0cd8f30bf6a359a
Aman Ahuja <trcxr16@hotmail.com>
2015-06-07 22:42:30 +0530

    Euler1 and Euler2
    Language C#

---
+++ Project Euler #1 Multiples of 3 and 5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Euler1
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            HashSet<Int64> list = new HashSet<Int64>();
+            Int64 mul3 = 0, mul5 = 0, mul15 = 0;
+            for (int i = 0; i < T; i++)
+            {
+                int N = int.Parse(Console.ReadLine());
+                N--;
+                Int64 a = N / 3;
+                Int64 b = N / 5;
+                Int64 c = N / 15;
+                mul3 = 3 * (a * (a + 1) / 2);
+                mul5 = 5 * (b * (b + 1) / 2);
+                mul15 = 15 * (c * (c + 1) / 2);
+                list.Add(mul3);
+                list.Add(mul5);
+                list.Add(mul15);
+                long sum = list.ElementAtOrDefault(0) + list.ElementAtOrDefault(1) - list.ElementAtOrDefault(2);
+                Console.WriteLine(sum);
+                list.Clear();
+            }
+        }
+    }
+}
---
+++ Project Euler #2 Even Fibonacci numbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.ProjectEuler
+{
+    class Euler2
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            List<UInt64> ar = new List<UInt64>();
+
+            for (int i = 0; i < T; i++)
+            {
+                UInt64 sum = 0;
+                UInt64 N = UInt64.Parse(Console.ReadLine());
+                ar.Add(1);
+                ar.Add(2);
+                int j = 2;
+
+                while (true)
+                {
+                    ar.Add(ar[j - 1] + ar[j - 2]);
+
+                    if (ar[j] >= N)
+                    {
+                        ar.Remove(ar[j]);
+                        break;
+                    }
+                    j++;
+                }
+
+                foreach (UInt64 item in ar)
+                {
+                    if (item % 2 == 0)
+                    {
+                        sum += item;
+                    }
+                }
+                Console.WriteLine(sum);
+                ar.Clear();
+            }
+        }
+    }
+}
